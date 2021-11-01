827cfa159ad619d9e983ab47058db77798e84717
constbar <const.bar@mail.ru>
2021-11-01 21:23:19 +0300

    d6

---
+++ srcs/mariadb/tools/set_sql.sh
@@ -0,0 +1,7 @@
+rc-service mariadb setup > /dev/null 2>&1
+openrc > /dev/null 2>&1 && rc-service mariadb start > /dev/null 2>&1
+mysql --execute="CREATE DATABASE $DATABASE;"
+mysql --execute="GRANT ALL PRIVILEGES ON wordpress.* TO '$USER'@'%' IDENTIFIED BY '$PASSWORD';"
+mysql --execute="FLUSH PRIVILEGES;"
+mysql $DATABASE < wp.sql
+mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD'; FLUSH PRIVILEGES;"
